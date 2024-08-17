eff6e21fd3cc0a6eac34f807d16129071f4e2995
HEKYPTO <tsunyanapat@gmail.com>
2024-08-17 19:59:27 +0700

    FIX imgae src

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,7 +4,7 @@ import styles from './card.module.css';
 const Card = () => {
     return (
         <div className={styles.card}>
-            <img src="https://www.cancer.gov/sites/g/files/xnrzdm211/files/styles/cgov_social_media/public/cgov_image/media_image/2023-04/syringe%20and%20bottle%20mrna%20vaccine.jpg" alt="Vaccine Information" />
+            <img src="vaccine.jpg" alt="Vaccine Information" />
             <div className={styles.content}>
                 <h2>Why Vaccines Matter</h2>
                 <p>Vaccines save lives by preventing the spread of infectious diseases. Make sure to stay up-to-date with your vaccinations.</p>
